6f63a4f5102fa63f5ff31072ff02b10cf542e64f
miiaevia <miiaevia@gmail.com>
2018-03-10 11:27:43 -0700

    menu options 1 and 2 work

--- src/AirField.java
+++ src/AirField.java
@@ -3,7 +3,21 @@ public class AirField {
 
 	private Jet[] jets = new Jet[10];
 	
+	CargoPlane cargo1 = new CargoPlane("747", 300.00, 3000, 34_000_000);
+	CargoPlane cargo2 = new CargoPlane("Guppy", 150.00, 1000, 50_000_000);
+	FighterJet fighter1 = new FighterJet("Zepplin", 80.00, 250, 25_000_000);
+	FighterJet fighter2 = new FighterJet("Albatros", 250.00, 1500, 60_000_000);
+	JetImpl jet1 = new JetImpl("Basic Jet", 100.00, 1000, 45_000_000);
+	
 	public AirField() {
+	}
+
+	public Jet[] getJets() {
+		return jets;
+	}
+
+	public void setJets(Jet[] jets) {
+		this.jets = jets;
 	}
 
 }
--- src/JetsApplication.java
+++ src/JetsApplication.java
@@ -1,23 +1,38 @@
 import java.util.Scanner;
 
 public class JetsApplication {
+	static int selection;
 	
-	private AirField airfield = new AirField();
 	Scanner kb = new Scanner(System.in);
 	
 	public JetsApplication() {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AirField airfield = new AirField();
+		Jet[] jets = airfield.getJets(); 
+//		launch(); 
+		displayUserMenu();
+		if (selection == 1) {
+			for (int i = 0; i < jets.length; i++) {
+				Jet jet = jets[i];
+				jet.toString();
+			}
+		}
+		else if (selection == 2) {
+			for (int i = 0; i < jets.length; i++) {
+				Jet jet = jets[i];
+				jet.fly();
+			}
+		}
 
 	}
 	
-	private void launch() {
-		
-	}
+//	private static void launch() {
+//		
+//	}
 	
-	private void displayUserMenu() {
+	private static void displayUserMenu() {
 		System.out.println("Choose of the following options: ");
 		System.out.println("\t1. List fleet");
 		System.out.println("\t2. Fly all jets");
@@ -25,6 +40,7 @@ public class JetsApplication {
 		System.out.println("\t4. View jet with longest range");
 		System.out.println("\t5. Add a jet to a Fleet");
 		System.out.println("\t6. Quit");
+		selection = kb.nextInt(); 
 	}
-
+	
 }
